422754ecc6eebae0e6095a965810329f4f9a3f78
Mitch Wenman <mitchwenman@gmail.com>
2018-12-16 14:25:39 +1030

    wip, broken

--- find_tour.py
+++ find_tour.py
@@ -10,8 +10,35 @@
 # A possible Eulerian tour would be [1, 2, 3, 1]
 
 def find_eulerian_tour(graph):
+    tour = []
+    workingGraph = list(graph) # so we can remove elements
     # your code here
-    return []
+    currNode = getFirstNode(workingGraph)
+    while len(workingGraph) != 0:
+        # find the next edge for the currentNode
+        nextEdge = getNextEdge(currNode, workingGraph)
+        print "Next Edge: " + str(nextEdge)
+        # update currNode
+        x, y = nextEdge
+        if currNode == x:
+            currNode = y
+        else:
+            currNode = x
+        # mark edge as traversed
+        tour.append(nextEdge)
+        workingGraph.remove(nextEdge)
+
+    return tour
+
+def getNextEdge(node, graph):
+    graphLength = len(graph)
+    if graphLength == 1:
+        return graph[0] # there's only one edge left
+    # find next edge containing node (not sure about this!!)
+    for edge in graph:
+        if node in edge:
+            # get next
+    return None
 
 def getDegrees(graph):
     degrees = {}
@@ -32,31 +59,55 @@ def countOdd(elementDict):
 def getFirstNode(graph):
     degrees = getDegrees(graph)
     if countOdd(degrees) == 0:
-        return graph[0] # start with any node, it doesn't matter
+        return graph[0][0] # start with any node, it doesn't matter
     else:
         # find first odd degree node
         for key in degrees.keys():
             if degrees[key] % 2 == 1:
                 return key
-    return None
+    return -1
+
+def graphA_Tests():
+    # simple graph (Graph A)
+    graph = [(1, 2), (2, 3), (3, 1)]
+    degrees = getDegrees(graph)
+    assert len(degrees) == 3
+    assert degrees[1] == 2 and degrees[2] == 2 and degrees[3] == 2
+    assert countOdd(degrees) == 0
+    firstEdge = getFirstNode(graph)
+    assert firstEdge == 1
+    tour = find_eulerian_tour(graph)
+    assert len(tour) == len (graph)
+
+def graphB_Tests():
+    # Graph #
+    # http://graphonline.ru/en/?graph=CwOwDenRQtayhfcd
+    graph = [(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9),
+                (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
+    degrees = getDegrees(graph)
+    assert len(degrees) == 10
+    assert degrees[5] == 4
+    assert countOdd(degrees) == 0
+    assert getFirstNode(graph) == 0
+    tour = find_eulerian_tour(graph)
+    assert len(tour) == len(graph)
+
+def graphC_Tests():
+    graph = [(1, 13), (1, 6), (6, 11), (3, 13),
+                (8, 13), (0, 6), (8, 9),(5, 9), (2, 6), (6, 10), (7, 9),
+                (1, 12), (4, 12), (5, 14), (0, 1),  (2, 3), (4, 11), (6, 9),
+                (7, 14),  (10, 13)]
+    degrees = getDegrees(graph)
+    assert len(degrees) == 15
+    assert getFirstNode(graph) == 1
+    tour = find_eulerian_tour(graph)
+    assert len(tour) == len(graph)
 
 def runTests():
-    # simple graph (Graph A)
-    graphA = [(1, 2), (2, 3), (3, 1)]
-    degreesA = getDegrees(graphA)
-    assert len(degreesA) == 3
-    assert degreesA[1] == 2 and degreesA[2] == 2 and degreesA[3] == 2
-    assert countOdd(degreesA) == 0
-    firstNode = getFirstNode(graphA)
-    assert firstNode == 1
+    graphA_Tests()
+    graphB_Tests()
+    graphC_Tests()
 
-    # Graph #
-    graphB = [(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9),
-                (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
-    degreesB = getDegrees(graphB)
-    assert len(degreesB) == 10
-    assert degreesB[5] == 4
-    assert countOdd(degreesB) == 0
 
 
 # main
