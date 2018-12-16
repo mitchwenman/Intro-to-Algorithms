6c879afd20299c249e9e8403f926886a43ad1f15
Mitch Wenman <mitchwenman@gmail.com>
2018-12-16 12:39:22 +1030

    added get degrees method

--- find_tour.py
+++ find_tour.py
@@ -21,11 +21,20 @@ def getDegrees(graph):
             degrees[node] = newDegree
     return degrees
 
+def countOdd(elementDict):
+    return 0
+
 
 def runTests():
+    # simple graph (Graph A)
     graphA = [(1, 2), (2, 3), (3, 1)]
     degrees = getDegrees(graphA)
     assert len(degrees) == 3
     assert degrees[1] == 2 and degrees[2] == 2 and degrees[3] == 2
+    assert countOdd(degrees) == 0
+
+    # Graph #
+    graphB = [(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9),
+                (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
 
 runTests()
